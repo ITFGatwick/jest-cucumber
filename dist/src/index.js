3652627af9da5cd9390f71e25a6949e143d06590
Matt Swarbrick <matt_swarbrick@hotmail.com>
2018-05-21 15:49:26 +0100

    v1.0.6 - included dist for github

---
+++ dist/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var parsed_feature_loading_1 = require("./parsed-feature-loading");
+exports.loadFeature = parsed_feature_loading_1.loadFeature;
+var feature_definition_creation_1 = require("./feature-definition-creation");
+exports.defineFeature = feature_definition_creation_1.defineFeature;
+//# sourceMappingURL=index.js.map
